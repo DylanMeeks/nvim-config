f7e81335944f2e4b0a12ca9e5080156762b9b77e
DylanMeeks <dylan.meeks@aol.com>
2023-06-26 09:47:19 -0500

    Adding comments

--- lua/dylan/remap.lua
+++ lua/dylan/remap.lua
@@ -1,9 +1,11 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+-- in visual mode, use J and K to move a selected up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- brings line below current to current line
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- <C-d>, <C-u>, n, and N now also center the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
@@ -33,13 +35,15 @@ end)
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- Enter Explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Sorce currunt file
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
