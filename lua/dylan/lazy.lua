cd1b32f9a3eaafacf7d9434894faf78066b67a31
Dylan Meeks <dylan.meeks@aol.com>
2023-08-27 13:47:11 -0500

    add markdown preview

--- lua/dylan/lazy.lua
+++ lua/dylan/lazy.lua
@@ -1,91 +1,103 @@
+--- Bootstrap Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = ' '
 
 local plugins = {
----------------------------------------------------------
--- telescope and related plugins
+    ---------------------------------------------------------
+    -- telescope and related plugins
     {
-	'nvim-telescope/telescope.nvim', version = '0.1.1',
-	dependencies = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim',
+        version = '0.1.1',
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
     'nvim-telescope/telescope-fzy-native.nvim',
 
     {
-      'sudormrfbin/cheatsheet.nvim',
+        'sudormrfbin/cheatsheet.nvim',
 
-        dependencies = {  {'nvim-lua/popup.nvim'}, }
+        dependencies = { { 'nvim-lua/popup.nvim' }, }
     },
----------------------------------------
--- Themes 
-   'EdenEast/nightfox.nvim' ,
+    ---------------------------------------
+    -- Themes
+    'EdenEast/nightfox.nvim',
 
-   "catppuccin/nvim", name = "catppuccin" ,
------------------------------------------
--- Treesitter 
-   { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
-   --   'nvim-treesitter/playground'
-------------------------------------- 
---harpoon
-   'ThePrimeagen/harpoon',
---------------------------------- 
---undotree
-   'mbbill/undotree',
-   'tpope/vim-fugitive',
-  -- Lua
+    "catppuccin/nvim",
+    name = "catppuccin",
+    -----------------------------------------
+    -- Treesitter
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    --   'nvim-treesitter/playground'
+    -------------------------------------
+    --harpoon
+    'ThePrimeagen/harpoon',
+    ---------------------------------
+    --undotree
+    'mbbill/undotree',
+    'tpope/vim-fugitive',
+    -- Lua
 
-   {
+    {
         "folke/trouble.nvim",
         dependencies = "nvim-tree/nvim-web-devicons",
     },
 
------------------------------------------------------- 
---LSP 
-   { 'VonHeikemen/lsp-zero.nvim',
-	    branch = 'v1.x',
-	    dependencies = {
-		    -- LSP Support
-		    {'neovim/nvim-lspconfig'},             -- Required
-		    {'williamboman/mason.nvim'},           -- Optional
-		    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    ------------------------------------------------------
+    --LSP
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		    -- Autocompletion
-		    {'hrsh7th/nvim-cmp'},         -- Required
-		    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		    {'hrsh7th/cmp-buffer'},       -- Optional
-		    {'hrsh7th/cmp-path'},         -- Optional
-		    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
-		    -- Snippets
-		    {'L3MON4D3/LuaSnip'},             -- Required
-		    {'rafamadriz/friendly-snippets'}, -- Optional
-	    }
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },             -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
     },
------------------------------------------------------ 
---Visuals and assorted dependencies
-   {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
+    -----------------------------------------------------
+    --Visuals and assorted dependencies
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        }
+    },
+    'ryanoasis/vim-devicons',
+    'nvim-lua/plenary.nvim',
+    ----------------------------------------------------
+    -- vim-be-good
+    'ThePrimeagen/vim-be-good',
+    ----------------------------------------------------
+    -- markdown-preview
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app && npm install",
+        init  = function() vim.g.mkdp_filetypes = { "markdown" } end,
+        ft = { "markdown" },
     }
-  },
-  'ryanoasis/vim-devicons',
-  'nvim-lua/plenary.nvim',
----------------------------------------------------- 
--- vim-be-good
-   'ThePrimeagen/vim-be-good',
 }
 
 local opts = {}
