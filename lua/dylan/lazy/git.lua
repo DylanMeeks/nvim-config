826fab345c4c65b99fa90554e8bdbc338be7e05e
DylanMeeks <dylan.meeks@aol.com>
2024-06-24 11:43:31 -0700

    feat: fugitive migrated, not currently in use. Added Neogit

--- lua/dylan/lazy/git.lua
+++ lua/dylan/lazy/git.lua
@@ -1,5 +1,5 @@
 return {
-    {
+    { -- only one of the following two needs to be enabled at a time
         "NeogitOrg/neogit",
         dependencies = {
             "nvim-lua/plenary.nvim",  -- required
@@ -7,14 +7,18 @@ return {
 
             -- Only one of these is needed, not both.
             "nvim-telescope/telescope.nvim", -- optional
-
-            "ibhagwan/fzf-lua",              -- optional
+            -- "ibhagwan/fzf-lua",              -- optional
         },
-        config = function ()
+        config = function()
             local neogit = require('neogit')
-            neogit.setup({
-                
-            })
+            neogit.setup({})
+            vim.keymap.set("n", "<leader>gs", function() neogit.open() end);
         end
-    }
+    },
+    --  {
+    --      "tpope/vim-fugitive",
+    --      config = function ()
+    --          vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+    --      end
+    --  },
 }
