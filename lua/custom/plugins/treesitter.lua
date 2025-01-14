66d9b3e9e6c9acd3dc09931212c3e34d2e3b7ae5
Dylan Meeks <dylan.meeks@aol.com>
2025-01-14 14:11:23 -0800

    feat: ghostty treesitter and updates

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,10 +1,14 @@
 return {
-    -- Treesitter
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        config = function()
-            require("custom.treesitter").setup()
-        end
-    },
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("custom.treesitter").setup()
+		end,
+	},
+	{
+		"bezhermoso/tree-sitter-ghostty",
+		build = "make nvim_install",
+	},
 }
