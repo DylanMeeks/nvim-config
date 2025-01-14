d58f1f08cd4dfb6b138040a9f14d7424bfb4261a
Dylan Meeks <dylan.meeks@aol.com>
2025-01-13 17:49:13 -0800

    feat: neogen changes

--- lua/custom/plugins/neogen.lua
+++ lua/custom/plugins/neogen.lua
@@ -1,11 +1,21 @@
 return {
-    {
-        "danymat/neogen",
-        -- Uncomment next line if you want to follow only stable versions
-        version = "*",
-        config = function ()
-           local neogen = require('neogen')
-           neogen.setup({})
-        end,
-    },
+	{
+		"danymat/neogen",
+		-- Uncomment next line if you want to follow only stable versions
+		version = "*",
+		config = function()
+			local neogen = require("neogen")
+			neogen.setup({ snippet_engine = "luasnip" })
+			local opts = { noremap = true, silent = true }
+			vim.keymap.set("n", "<Leader>nf", function()
+				require("neogen").generate({ type = "func" })
+			end, opts)
+			vim.keymap.set("n", "<Leader>nc", function()
+				require("neogen").generate({ type = "class" })
+			end, opts)
+			vim.keymap.set("n", "<Leader>nt", function()
+				require("neogen").generate({ type = "type" })
+			end, opts)
+		end,
+	},
 }
